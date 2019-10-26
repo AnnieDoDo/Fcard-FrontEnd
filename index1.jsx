d790632cd4e291b788da37a67d9eb9a69f2fe72f
AnnieDoDo <r8323467r@gmail.com>
2019-10-27 02:05:47 +0800

    MainPage NavBar Background

--- index1.jsx
+++ index1.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import MainPage from './components/mainpage/mainpage.jsx'
 import Login from './components/login/login.jsx';
 
 ReactDOM.render(
-    <Login />
+    <MainPage />
 , document.getElementById('root'));
