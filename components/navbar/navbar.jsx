7b350948c17754d14ca8218dd08bed8bf909f14c
AnnieDoDo <r8323467r@gmail.com>
2019-10-27 20:54:02 +0800

    navbar crow

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Style from '../navbar/navbar.css'
 import {Navbar} from "react-bootstrap"
+import {Nav} from "react-bootstrap"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCrow } from '@fortawesome/free-solid-svg-icons'
 
 class NavBar extends React.Component {
     constructor(props){
@@ -8,8 +11,13 @@ class NavBar extends React.Component {
     };
     render() {
         return (
-            <Navbar  variant="light" className = "navbarBackground" >
+            <Navbar variant="dark" className = "navbarBackground" >
                 <Navbar.Brand>Fcard</Navbar.Brand>
+                <Nav className="ml-auto">
+                    <Nav.Link href="#" >
+                        <FontAwesomeIcon icon={faCrow} />
+                    </Nav.Link>
+                </Nav>
             </Navbar>
         );
     }
